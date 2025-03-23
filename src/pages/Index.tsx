13fcd4ba680fd460b54a2a9c8f20a608ce8cf936
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-03-23 18:19:05 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -66,7 +66,7 @@ const Index = () => {
       <footer className="border-t border-gray-300 py-8 bg-white">
         <div className="container mx-auto px-4 md:px-6 text-center text-[#828282] text-sm">
           <p>
-            &copy; {new Date().getFullYear()} BlogDomain. All rights reserved.
+            &copy; {new Date().getFullYear()} MoneyWorth. All rights reserved.
           </p>
         </div>
       </footer>
