81700cc9f7084e0be3593e69fea1d30c44bb1dfb
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-03-24 01:08:45 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,7 +39,7 @@ const Header = () => {
     >
       <div className="container mx-auto px-4 md:px-6 flex items-center justify-between">
         <Link to="/" className="flex items-center gap-2 font-bold text-2xl">
-          <img src="/MoneyWorth.png" alt="MoneyWorth Logo" className="h-6" />
+          <img src="/MoneyWorth.webp" alt="MoneyWorth Logo" className="h-6" />
         </Link>
 
         {/* Desktop Navigation */}
@@ -78,7 +78,7 @@ const Header = () => {
               location.pathname === "/tax-calculator" && "font-medium text-gray-900"
             )}
           >
-            Tax Calculator
+            Paye Calculator
           </Link>
           <Link
             to="/about"
