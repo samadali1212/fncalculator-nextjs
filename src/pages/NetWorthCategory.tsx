98b8d04314acf9448c9dcb5578c7125a0605a575
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-04-06 00:21:41 +0300

    Update NetWorthCategory.tsx

--- src/pages/NetWorthCategory.tsx
+++ src/pages/NetWorthCategory.tsx
@@ -312,7 +312,7 @@ const NetWorthCategory = () => {
       <footer className="border-t border-gray-300 py-8 bg-white">
         <div className="container mx-auto px-4 md:px-6 text-center text-[#828282] text-sm">
           <p>
-            &copy; {new Date().getFullYear()} financepedia. All rights reserved.
+            &copy; {new Date().getFullYear()} SalaryList. All rights reserved.
           </p>
         </div>
       </footer>
