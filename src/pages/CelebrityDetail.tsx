6c905dd703ce2cd38ab68ad0e64080799942ea7f
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-04-09 00:00:04 +0300

    Update CelebrityDetail.tsx

--- src/pages/CelebrityDetail.tsx
+++ src/pages/CelebrityDetail.tsx
@@ -182,7 +182,7 @@ const CelebrityDetail = () => {
                 </div>
                 
                 <div className="text-xl font-semibold text-[#333]">
-                  {formatSalary(celebrity.salary, celebrity.currency)}
+                  Salary is {formatSalary(celebrity.salary,"R")} per month
                 </div>
                 
                 <div className="flex items-center mt-1">
