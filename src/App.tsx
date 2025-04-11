eefbdcb90ed0144deb7cf6de1a1ca9f400945da8
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-04-11 17:31:52 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import About from "./pages/About";
+import BlogPost from "./pages/BlogPost";
 import Salaries from "./pages/Salaries";
 import JobDetail from "./pages/JobDetail";
 import Celebrities from "./pages/Celebrities";
@@ -35,6 +36,7 @@ const App = () => (
           <Route path="/branch-codes" element={<Navigate to="/" replace />} />
           <Route path="/branch-codes/:slug" element={<BranchCodeDetail />} />
           <Route path="/about" element={<About />} />
+          <Route path="/post" element={<BlogPost />} />
           <Route path="/salaries" element={<Salaries />} />
           <Route path="/salaries/:jobId" element={<JobDetail />} />
           <Route path="/celebrities" element={<Celebrities />} />
