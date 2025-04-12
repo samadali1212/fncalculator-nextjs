f9ec57cf94afc267e8d46a5905a7d969d951f919
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-04-13 00:26:32 +0300

    Update BranchCodes.tsx

--- src/pages/BranchCodes.tsx
+++ src/pages/BranchCodes.tsx
@@ -92,6 +92,10 @@ const BranchCodes = () => {
           </div>
         </div>
 
+              <div className="container mx-auto px-4 pb-8">
+        <AdSense slot="9803570345" format="horizontal" className="py-4" />
+      </div>
+
         <motion.div 
           className="mb-6 grid grid-cols-1 md:grid-cols-3 gap-4"
           initial={{ opacity: 0, y: 20 }}
