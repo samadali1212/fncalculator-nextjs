4937bb44f6618f00bac1415eccaaf541853c0f0c
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-04-12 12:21:37 +0300

    Update BlogDetail.tsx

--- src/pages/BlogDetail.tsx
+++ src/pages/BlogDetail.tsx
@@ -72,7 +72,7 @@ const BlogDetail = () => {
       className="min-h-screen bg-[#f6f6f0]"
     >
       <SEO 
-        title={`${blogPost.title} | Financepedia Blog`} 
+        title={`${blogPost.title} | Sassa Insider`} 
         description={blogPost.excerpt}
         canonicalUrl={`/blog/${blogPost.slug}`}
       />
@@ -247,7 +247,7 @@ const BlogDetail = () => {
       <footer className="border-t border-gray-300 py-8 bg-white">
         <div className="container mx-auto px-4 md:px-6 text-center text-[#828282] text-sm">
           <p>
-            &copy; {new Date().getFullYear()} Financepedia. All rights reserved.
+            &copy; {new Date().getFullYear()} Sassa Insider. All rights reserved.
           </p>
         </div>
       </footer>
