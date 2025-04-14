4802b95d8aacd980eeadb59161282f6f52483aa0
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-04-14 10:06:46 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Layout from "./components/Layout";
 import About from "./pages/About";
 import Salaries from "./pages/Salaries";
 import JobDetail from "./pages/JobDetail";
+import HourlyRates from "./pages/HourlyRates";
+import HourlyRateDetail from "./pages/HourlyRateDetail";
+import TaxCalculator from "./pages/TaxCalculator";
+import TaxCalculationDetail from "./pages/TaxCalculationDetail";
 import Celebrities from "./pages/Celebrities";
 import CelebrityDetail from "./pages/CelebrityDetail";
 import NetWorth from "./pages/NetWorth";
@@ -33,6 +37,14 @@ const App = () => (
         <Route path="/branch-codes/:slug" element={<BranchCodeDetail />} />
         <Route path="/about" element={<About />} />
         <Route path="/salaries" element={<Salaries />} />
+        <Route path="/hourly-rates" element={<HourlyRates />} />
+        <Route path="/hourly-rates/:rateId" element={<HourlyRateDetail />} />
+        <Route path="/tax-calculator" element={<TaxCalculator />} />
+        <Route path="/tax-calculator/monthly" element={<TaxCalculator />} />
+        <Route path="/tax-calculator/yearly" element={<TaxCalculator />} />
+        <Route path="/tax-calculator/monthly/:incomeId" element={<TaxCalculationDetail />} />
+        <Route path="/tax-calculator/yearly/:incomeId" element={<TaxCalculationDetail />} />
+        <Route path="/tax-calculator/:incomeId" element={<TaxCalculationDetail />} />
         <Route path="/salaries/:jobId" element={<JobDetail />} />
         <Route path="/celebrities" element={<Celebrities />} />
         <Route path="/celebrities/:slug" element={<CelebrityDetail />} />
