eedc02698388eff17cdbaa37301197876d8db5d4
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-04-16 10:43:40 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Layout from "./components/Layout";
 import About from "./pages/About";
 import Salaries from "./pages/Salaries";
 import JobDetail from "./pages/JobDetail";
+import HourlyRates from "./pages/HourlyRates";
+import HourlyRateDetail from "./pages/HourlyRateDetail";
+import TaxCalculator from "./pages/TaxCalculator";
+import TaxCalculationDetail from "./pages/TaxCalculationDetail";
 import Celebrities from "./pages/Celebrities";
 import CelebrityDetail from "./pages/CelebrityDetail";
 import NetWorth from "./pages/NetWorth";
@@ -27,12 +31,12 @@ const App = () => (
     <Layout>
       <Routes>
         {/* Set BranchCodes as the explicit homepage */}
-        <Route path="/" element={<BranchCodes />} />
+        <Route path="/" element={<Salaries />} />
         {/* Ensure branch-codes path also works and doesn't create duplicate content */}
-        <Route path="/branch-codes" element={<Navigate to="/" replace />} />
+        <Route path="/salaries" element={<Navigate to="/" replace />} />
         <Route path="/branch-codes/:slug" element={<BranchCodeDetail />} />
         <Route path="/about" element={<About />} />
-        <Route path="/salaries" element={<Salaries />} />
+        <Route path="/branch-codes" element={<BranchCodes />} />
         <Route path="/salaries/:jobId" element={<JobDetail />} />
         <Route path="/celebrities" element={<Celebrities />} />
         <Route path="/celebrities/:slug" element={<CelebrityDetail />} />
@@ -52,6 +56,16 @@ const App = () => (
         {/* Blog Routes */}
         <Route path="/blog" element={<Blog />} />
         <Route path="/blog/:slug" element={<BlogDetail />} />
+        
+        {/* Tax Calculator & Hourly Rates Routes */}
+        <Route path="/hourly-rates" element={<HourlyRates />} />
+        <Route path="/hourly-rates/:rateId" element={<HourlyRateDetail />} />
+        <Route path="/tax-calculator" element={<TaxCalculator />} />
+        <Route path="/tax-calculator/monthly" element={<TaxCalculator />} />
+        <Route path="/tax-calculator/yearly" element={<TaxCalculator />} />
+        <Route path="/tax-calculator/monthly/:incomeId" element={<TaxCalculationDetail />} />
+        <Route path="/tax-calculator/yearly/:incomeId" element={<TaxCalculationDetail />} />
+        <Route path="/tax-calculator/:incomeId" element={<TaxCalculationDetail />} />
         
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
         <Route path="*" element={<NotFound />} />
