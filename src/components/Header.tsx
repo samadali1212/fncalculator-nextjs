0bd208bee61497c9bf827d13fe62a12768f81e11
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-04-18 16:49:37 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
 
   const menuItems = [
     { path: "/categories/general-knowledge", label: "Rankings" },
-    { path: "/celebrities", label: "celebs Salary" },
+    { path: "/celebrities", label: "Celebs Salary" },
     { path: "/net-worth", label: "Net Worth" },
     { path: "/hourly-rates", label: "Hourly Rates" },
     { path: "/tax-calculator", label: "Tax Calculator" },
