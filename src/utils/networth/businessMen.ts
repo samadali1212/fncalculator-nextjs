b8d53d12ed7e28bafc638261437b4130bfecfd4e
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-04-22 14:56:25 +0300

    Create businessMen.ts

---
+++ src/utils/networth/businessMen.ts
@@ -0,0 +1,40 @@
+import { NetWorthPerson } from './index';
+
+export const businessMen: NetWorthPerson[] = [
+  {
+    id: "business-1",
+    name: "Jordan Ullman",
+    gender: "male",
+    netWorth: 4000000,
+    currency: "USD",
+    occupation: "Disc jockey",
+    dateOfBirth: "1993-09-08",
+    country: "Canada",
+    industry: "Disc jockey",
+    company: "XO Records",
+    description: "Jordan Ullman is a Canadian music producer who has a net worth of $4 million. Jordan Ullman was born in Toronto, Ontario, Canada in September 1993. He is best known as the producer in the R&B duo Majid Jordan with singer Majid Al Maskati. The duo formed in 2011 and released their self-titled debut studio album in 2016 which reached #6 on the US R&B chart and #20 in Canada.",
+    source: "Celebrity Net Worth",
+    lastUpdated: "2025-04-04",
+    imageUrl: "https://vz.cnwimg.com/thm/JiZA76w0xkNFCDe0kfKmqnfETss=/400x400/center/top/filters:format(webp)/2019/02/jord.jpg",
+    slug: "jordan-ullman-net-worth",
+    categories: ["celebrities", "djs"]
+  },
+  {
+    id: "business-2",
+    name: "Gianluca Vacchi",
+    gender: "male",
+    netWorth: 200000000,
+    currency: "USD",
+    occupation: "Disc jockey",
+    dateOfBirth: "1967-06-05",
+    country: "Italy",
+    industry: "Disc jockey",
+    company: "Independent",
+    description: "Gianluca Vacchi is an Italian social media personality, entrepreneur, DJ, and author who has a net worth of $200 million. Gianluca earned his fortune as a large stock holder in an Italian manufacturing business called IMA. As on of the company's largest individual shareholders, Vacchi earns tens of million in dividends every year. More on that in a moment.",
+    source: "Forbes",
+    lastUpdated: "2025-04-05",
+    imageUrl: "https://vz.cnwimg.com/thm/pSKfZKipI9jvKm2SkUusxB55zBM=/600x300/center/top/filters:format(webp)/2018/12/Gianluca-Vacchi.jpg",
+    slug: "gianluca-vacchi-net-worth",
+    categories: ["celebrities", "djs"]
+  }
+];
