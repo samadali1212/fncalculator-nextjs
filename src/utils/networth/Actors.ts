f06a807eae7f367e1fc54d6f6ae6f96e7bae5a62
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-04-22 14:42:58 +0300

    Create Actors.ts

---
+++ src/utils/networth/Actors.ts
@@ -0,0 +1,58 @@
+import { NetWorthPerson } from './index';
+
+export const actors: NetWorthPerson[] = [
+{
+    id: 6,
+    name: "Air Force Amy",
+    gender: "female",
+    netWorth: 1000000,
+    currency: "USD",
+    occupation: "Model",
+    dateOfBirth: "1968-08-05",
+    country: "United States of America",
+    industry: "Adult Entertainment",
+    company: "Moonlite BunnyRanch",
+    description: "Air Force Amy net worth and earnings: Air Force Amy is an American licensed prostitute, adult star, model, and documentary subject who has a net worth of $1 million. Air Force Amy began working as a licensed prostitute in Nevada in 1990. She applied to work at Nevada's famous Chicken Ranch brothel just prior to being discharged from the United States Air Force. She became somewhat of a legend in Nevada. She worked in such brothels as the Mustang Ranch, the Sagebrush Ranch, the Cherry Patch Ranch, and the Moonlite Bunny Ranch. In 2000, she began appearing in adult magazines, and as a featured speaker on a variety of programs and specials about prostitution and brothel life. She was a subject of the HBO documentary, \"Cathouse: The Series\". After taking a leave of absence to deal with back issues, she chose to return to working full time at the Moonlite Bunny Ranch in 2013.",
+    source: "Idol Net Worth",
+    lastUpdated: "2025-01-06",
+    imageUrl: "https://vz.cnwimg.com/thm/bMVVlzpc3kWiFvv7h8KSh8dSlcY=/400x400/center/top/filters:format(webp)/2013/09/amy.jpg",
+    slug: "air-force-amy-net-worth",
+    categories: ["celebrities", "models"]
+  },
+  {
+    id: 7,
+    name: "Monique Alexander",
+    gender: "female",
+    netWorth: 2000000,
+    currency: "USD",
+    occupation: "Pornographic actor, Actor, Model",
+    dateOfBirth: "1982-05-26",
+    country: "United States of America",
+    industry: "Adult Entertainment",
+    company: "Vivid Entertainment",
+    description: "Monique Alexander is an American pornographic actress and model who has a net worth of $2 million. Monique Alexander was born in Vallejo, California in May 1982. She started working as a stripper at 18 years old and began appearing in adult magazines before getting into the adult film industry. Monique Alexander won AVN Awards in 2008, 2009, and 2011 and was inducted into the AVN Hall of Fame in 2017.",
+    source: "Wealthy Gorilla",
+    lastUpdated: "2025-01-07",
+    imageUrl: "https://vz.cnwimg.com/thm/8S31NTPP8GqK0oW5rdACp648c54=/400x400/center/top/filters:format(webp)/2018/05/ma.jpg",
+    slug: "monique-alexander-net-worth",
+    categories: ["celebrities", "models"]
+  },
+  {
+    id: 8,
+    name: "Abella Anderson",
+    gender: "female",
+    netWorth: 600000,
+    currency: "USD",
+    occupation: "Pornographic actor",
+    dateOfBirth: "1988-05-16",
+    country: "United States of America",
+    industry: "Adult Entertainment",
+    company: "Bang Bros",
+    description: "Abella Anderson is an Cuban-American adult film star who has a net worth of $600 thousand. Abella Anderson was born on May 16, 1988 in Cuba. She moved to the United States with her family at a very young age. Abella Anderson has appeared in more than 30 adult movies in her short career.",
+    source: "Idol Net Worth",
+    lastUpdated: "2025-01-08",
+    imageUrl: "https://vz.cnwimg.com/thm/G2YG6n7cXm2irvpCPxkJUiKvUGI=/400x400/center/top/filters:format(webp)/2013/07/abella.jpg",
+    slug: "abella-anderson-net-worth",
+    categories: ["celebrities", "models"]
+  }
+];
