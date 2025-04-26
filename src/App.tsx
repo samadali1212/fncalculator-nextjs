14b71365b6a016166d9509623aa59876cf3887a5
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-04-26 11:13:20 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,12 @@ import GlobalNetWorthDetail from "./pages/GlobalNetWorthDetail";
 import GlobalNetWorthCategory from "./pages/GlobalNetWorthCategory";
 import GlobalStandaloneCategories from "./pages/GlobalStandaloneCategories";
 
+// Price pages
+import Price from "./pages/Price";
+import PriceDetail from "./pages/PriceDetail";
+import StandalonePrice from "./pages/StandalonePrice";
+import PriceCategory from "./pages/PriceCategory";
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -68,6 +74,12 @@ const App = () => {
           {/* Blog Routes */}
          <Route path="/blog" element={<Blog />} />
          <Route path="/blog/:slug" element={<BlogDetail />} />
+
+          {/* Price Routes */}
+            <Route path="/price" element={<Price />} />
+            <Route path=":slug" element={<PriceDetail />} />
+            <Route path="category/:slug" element={<PriceCategory />} />      
+            <Route path="/price-categories" element={<StandalonePrice />} />
                   
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
