8fb3a934a76b966004f6090a184fa89bc56482a3
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-06-08 08:19:46 +0300

    Create index.ts

---
+++ src/utils/jobData/index.ts
@@ -0,0 +1,27 @@
+
+import { Job, JobLocation, JobCategory } from './types';
+import { educationJobs } from './educationJobs';
+import { administrationJobs } from './administrationJobs';
+
+// Combine all job data
+export const jobsData: Job[] = [
+  ...educationJobs,
+  ...administrationJobs
+];
+
+// Get unique locations from job data
+export const getUniqueLocations = (): JobLocation[] => {
+  const locationSet = new Set<JobLocation>();
+  jobsData.forEach(job => locationSet.add(job.location));
+  return Array.from(locationSet);
+};
+
+// Get unique categories from job data
+export const getUniqueCategories = (): JobCategory[] => {
+  const categorySet = new Set<JobCategory>();
+  jobsData.forEach(job => categorySet.add(job.category));
+  return Array.from(categorySet);
+};
+
+// Export types
+export type { Job, JobLocation, JobCategory, ApplicationMethod } from './types';
