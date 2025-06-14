fa451c82ca13b6348f8bf58f84ed3b6ba55a1598
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-06-14 10:07:45 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import EventPage from "./pages/EventPage";
 import YearPage from "./pages/YearPage";
 import Blog from "./pages/Blog";
 import BlogDetail from "./pages/BlogDetail";
+import Paye from "./pages/Paye";
+import PayeDetail from "./pages/PayeDetail";
 import MonthPageWrapper from "./components/MonthPageWrapper";
 
 const App = () => {
@@ -41,6 +43,14 @@ const App = () => {
             {/* Blog Routes */}
             <Route path="/blog" element={<Blog />} />
             <Route path="/blog/:slug" element={<BlogDetail />} />
+
+          {/* Tanzania PAYE Calculator Routes */}
+          <Route path="/paye" element={<Paye />} />
+          <Route path="/paye/monthly" element={<Paye />} />
+          <Route path="/paye/yearly" element={<Paye />} />
+          <Route path="/paye/monthly/:incomeId" element={<PayeDetail />} />
+          <Route path="/paye/yearly/:incomeId" element={<PayeDetail />} />
+          <Route path="/paye/:incomeId" element={<PayeDetail />} />
                         
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
