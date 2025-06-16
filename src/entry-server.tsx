a03d081ebd1a6dc99c94b0bffaf328cb0f868f81
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-06-16 12:01:21 +0300

    Update entry-server.tsx

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,12 +1,24 @@
-import { StrictMode } from 'react'
-import { renderToString } from 'react-dom/server'
-import App from './App'
 
-export function render(_url: string) {
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from "react-router-dom/server";
+import { HelmetProvider } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from './App';
+
+export async function render(url: string) {
+  // Create a fresh query client for each request
+  const queryClient = new QueryClient();
+  const helmetContext = {};
+
   const html = renderToString(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  )
-  return { html }
+    <HelmetProvider context={helmetContext}>
+      <QueryClientProvider client={queryClient}>
+        <StaticRouter location={url}>
+          <App />
+        </StaticRouter>
+      </QueryClientProvider>
+    </HelmetProvider>
+  );
+
+  return html;
 }
