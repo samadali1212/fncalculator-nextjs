80ec5ed591059ee8febd21b7b6baf6e95ad69367
Ansam <52356127+samadali1212@users.noreply.github.com>
2025-06-19 15:35:01 +0300

    Create entry-client.tsx

---
+++ src/entry-client.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { hydrateRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from './App';
+import './index.css';
+
+const queryClient = new QueryClient();
+
+hydrateRoot(
+  document.getElementById('root')!,
+  <React.StrictMode>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </HelmetProvider>
+  </React.StrictMode>
+);
